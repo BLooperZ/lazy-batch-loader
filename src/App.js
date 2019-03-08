2b09f61d21707816237014ffc2ce649ad1e47b88
BLooperZ <blooperz@users.noreply.github.com>
2019-03-08 02:57:29 +0200

    simplify example usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 
 import LazyBatchLoader from './components/LazyBatchLoader'
 
 const ListRenderer = ({ items }) => (
-    <div>
+    <>
         { items.map((item, idx) => <p key={idx}>{item}</p>) }
-    </div>
+    </>
 )
 
 const LazyListRenderer = LazyBatchLoader({ batchSize: 15, interval: 1000 })(ListRenderer)
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <LazyListRenderer items={ Array.from({ length: 1000 }, (x, idx) => idx) } />
-            </div>
-        )
-    }
-}
+const App = () => (
+    <div className="App">
+        <LazyListRenderer items={ Array.from({ length: 1000 }, (x, idx) => idx) } />
+    </div>
+)
 
 export default App
